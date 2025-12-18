da1f1ad9e9bebaa3d492cb00c7d6adda76a2d825
Simon Burton <simon@arrowtheory.com>
2025-12-18 20:05:19 +0000

    commit edits

--- qumba/matroid.py
+++ qumba/matroid.py
@@ -14,6 +14,8 @@ from operator import add
 
 import numpy
 
+from z3 import Bool, And, Or, Xor, Not, Implies, Sum, If, Solver
+
 from qumba.action import mulclose
 from qumba.symplectic import SymplecticSpace
 from qumba.matrix import Matrix
@@ -117,7 +119,6 @@ class Matroid:
 
 def all_matroids(n):
 
-    from z3 import Bool, And, Or, Xor, Not, Implies, Sum, If, Solver
     bits = list(numpy.ndindex((2,)*n))
 
     # is this subset an independent set?
@@ -192,6 +193,130 @@ def all_matroids(n):
     #print("all_matroids(%d) = %d" % (n, count))
 
 
+def all_sp_matroids(n):
+    # See: Theorem 3.8.1 in [Borovik,Gelfand,White]
+
+    nn = 2*n
+    # We use ziporder: (2*i,2*i+1) are the symplectic pairs
+
+    # all admissable sets as bitvectors on 2*n elements
+    bits = []
+    for vec in numpy.ndindex((3,)*n):
+        mask = [0]*nn
+        for i in range(n):
+            mask[2*i:2*i+2] = [[0,0], [0,1], [1,0]][vec[i]]
+        mask = tuple(mask)
+        bits.append(mask)
+        
+    # is this subset an independent set?
+    lookup = {} # variables
+    for a in bits:
+        name = "v"+''.join(str(i) for i in a)
+        v = Bool(name)
+        lookup[a] = v
+
+    solver = Solver()
+    Add = solver.add
+    #def Add(term):
+    #    print("Add: %s"%(term))
+    #    solver.add(term)
+
+    def le(a, b):
+        for i in range(nn):
+            if a[i]>b[i]:
+                return False
+        return True
+
+    pairs = [(a,b) for a in bits for b in bits]
+    for a in bits:
+        if sum(a)==0:
+            Add(lookup[a]) # empty set
+
+    # down-closed
+    for (a,b) in pairs:
+        if a!=b and le(a,b):
+            Add( If(lookup[b], lookup[a], True) )
+
+    star = lambda i : [i+1, i-1][i%2]
+    for i in range(nn):
+        assert 0<=star(i)<nn
+
+    #print("symplectic exchange:")
+    for (X,Y) in pairs:
+        #print("pair:", X, Y)
+        if sum(X) >= sum(Y):
+            continue
+        # |X| < |Y|
+        src = And(lookup[X], lookup[Y]) # "if X and Y are independent sets"
+        terms = []
+        for i in range(nn):
+            if Y[i] and not X[i]:
+                Z = list(X)
+                Z[i] = 1
+                Z = tuple(Z)
+                if Z in lookup: # Z admissable
+                    terms.append(lookup[Z])
+        assert terms
+        Z = tuple(max(i,j) for (i,j) in zip(X,Y)) # Z = XuY
+        if Z not in lookup:
+            #print("XuY inadmissable", X, Y, Z)
+            #items = []
+            for i in range(nn):
+                if Z[i]:
+                    continue
+                Xi = list(X)
+                Xi[i] = 1
+                Xi = tuple(Xi)
+                if Xi not in lookup:
+                    continue
+                Yi = [0]*nn
+                for j in range(n):
+                    if X[j] and not Y[star(j)]:
+                        Yi[j] = 1
+                assert Yi[i] == 0
+                Yi[i] = 1
+                Yi = tuple(Yi)
+                assert Yi in lookup
+                t = And(lookup[Xi], lookup[Yi])
+                #print("\t", t)
+                terms.append(t)
+            #terms.append
+        Add( If(src, Or(*terms), True) )
+
+    #return
+    
+    count = 0
+    matroids = set()
+    while 1:
+        result = solver.check()
+        if str(result) != "sat":
+            break
+
+        model = solver.model()
+    
+        sol = {a:model.get_interp(lookup[a]) for a in bits}
+        found = []
+        for (k,v) in sol.items():
+            if v:
+                found.append(k)
+        M = Matroid(nn, found)
+        M.check()
+        assert M not in matroids
+        matroids.add(M)
+        yield M
+
+        term = []
+        for a in bits:
+            if sol[a]:
+                term.append(Not(lookup[a]))
+            else:
+                term.append((lookup[a]))
+        term = Or(*term)
+        Add(term)
+        count += 1
+    #print("all_matroids(%d) = %d" % (n, count))
+
+
 def all_orbits(n):
 
     from bruhat.gset import Group, Perm
@@ -210,34 +335,14 @@ def all_orbits(n):
     return orbits
 
 
-
-#def test_rank():
-#
-#    n = 3
-#
-#    from qumba.graph_states import render_func
-#    cvs = Canvas()
-#
-#    x = y = 0
-#    matroids = list(all_matroids(n))
-#    matroids.sort(key = lambda m:(m.rank, len(m.masks)))
-#    for idx,M in enumerate(matroids):
-#        print(M)
-#
-#        f = []
-#        for mask in M.all_masks():
-#            R = M.restrict(mask)
-#            R.check()
-#            #print("\t%s : %d"%(mask, R.rank))
-#            f.append(R.rank)
-#        fg = render_func(n, f)
-#        cvs.insert(x, y, fg)
-#        #cvs.text(x, y, str(M))
-#        for mi,mask in enumerate(M.masks):
-#            cvs.text(x, y-0.5*mi, str(mask))
-#        x += fg.get_bound_box().width
-#
-#    cvs.writePDFfile("matroid_%d.pdf"%n)
+def test_sp():
+    for n in [1,2,3,4]:
+        count = 0
+        found = {i:[] for i in range(n+1)}
+        for M in all_sp_matroids(n):
+            #print(M.rank, end=' ')
+            found[M.rank].append(M)
+        print(n, [len(found[i]) for i in range(n+1)])
 
     
 def test_matroids():
