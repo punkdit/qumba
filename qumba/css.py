4b99d83b41652fc04385fbf680384521c293a77b
Simon Burton <simon@arrowtheory.com>
2026-05-15 17:24:48 +0100

    commit edits

--- qumba/css.py
+++ qumba/css.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+from functools import reduce
+from operator import lshift
+
 from qumba.argv import argv
 from qumba.matrix import Matrix
 from qumba.lin import identity2, zeros2
 from qumba.util import cache
-from qumba import csscode, qcode
+from qumba import csscode, qcode, construct
 from qumba.smap import SMap
 
 
@@ -289,6 +292,195 @@ IIIIZIIIIIIZIZIZIIIIZZIIIIIIIZZI
     print(H.sum(0))
     print(H.sum(1))
 
+
+
+def main_tensor():
+    H = Matrix([[1,1]])
+    Ht = H.t
+
+    m, n = H.shape
+    concatenate = Matrix.concatenate
+    identity = Matrix.identity
+    zeros = Matrix.zeros
+
+    I1 = identity(1)
+    I2 = identity(2)
+
+    H0 = concatenate(H@I2@I1, I2@H@I1, I2@I2@Ht)
+
+    H1 = concatenate(
+        concatenate(I1@H@I1, H@I1@I1, zeros((1, 8)), axis=1),
+        concatenate(I1@I2@Ht, zeros((4,2)), H@I2@I2, axis=1),
+        concatenate(zeros((4,2)), I2@I1@Ht, I2@H@I2, axis=1))
+
+    assert (H1*H0).is_zero()
+
+    H2 = concatenate(I1@I1@Ht, I1@H@I2, H@I1@I2, axis=1)
+    assert (H2*H1).is_zero()
+
+    Hx = H0.t
+    Hz = H1
+    css = csscode.CSSCode(Hx=Hx.A, Hz=Hz.A)
+    css.bz_distance()
+    print(css)
+
+    dual = css.get_dual()
+    code = css+css+css
+    print(code)
+
+    from qumba.gcolor import dump_transverse
+    dump_transverse(code.Hx, code.Lx)
+
+    return
+
+    Hx = H1.t
+    Hz = H2
+    css = csscode.CSSCode(Hx=Hx.A, Hz=Hz.A)
+    css.bz_distance()
+    print(css)
+    print(css.longstr())
+
+
+def parse(n, stabs):
+    import numpy
+    #print(stabs)
+    stabs = stabs.replace(" ", "")
+    stabs = stabs.replace("\n", "")
+    stabs = stabs.split(",")
+    x_ops = []
+    z_ops = []
+    for stab in stabs:
+        if "X" in stab:
+            s = stab.replace("X", " ")
+            ops = x_ops
+        elif "Z" in stab:
+            s = stab.replace("Z", " ")
+            ops = z_ops
+        else:
+            assert 0
+        idxs = s.split(" ")
+        idxs = [int(i)-1 for i in idxs if len(i)]
+        #print(idxs)
+        op = [0]*n
+        for i in idxs:
+            op[i] = 1
+        ops.append(op)
+    Hx = numpy.array(x_ops)
+    Hz = numpy.array(z_ops)
+    #print(Hx)
+    css = csscode.CSSCode(Hx=Hx, Hz=Hz)
+    css.bz_distance()
+    return css
+        
+    
+
+
+def test_vasmer():
+    # https://arxiv.org/abs/1801.04255
+    # Appendix A
+    n = 12
+    stabs = """
+    X5X6X7X8X9X10X11X12,
+    X1X3X5, X2X4X7,
+    Z6Z9, Z6Z10,
+    Z8Z11, Z8Z12,
+    Z1Z5Z6, Z2Z6Z7,
+    Z4Z7Z8, Z3Z5Z8"""
+
+    C0 = parse(n, stabs)
+    print(C0)
+    #print(C0.longstr())
+
+    stabs = """
+    X1X5X6X9, X2X6X7X10,
+    X3X5X8X11, X4X7X8X12,
+    Z1Z3Z5, Z1Z2Z6,
+    Z2Z4Z7, Z3Z4Z8,
+    Z5Z9Z11, Z6Z9Z10,
+    Z7Z10Z12"""
+
+    C1 = parse(n, stabs)
+    print(C1)
+    #print(C1.longstr())
+
+    stabs = """
+    X1X2X3X4X5X6X7X8,
+    X6X9X10, X8X11X12,
+    Z1Z5, Z3Z5,
+    Z2Z7, Z4Z7,
+    Z5Z8Z11, Z5Z6Z9,
+    Z6Z7Z10, Z7Z8Z12"""
+
+    C2 = parse(n, stabs)
+    print(C2)
+    #print(C2.longstr())
+
+    SymplecticSpace = qcode.SymplecticSpace
+
+    cube = construct.get_832()
+    print(cube)
+
+    right = C0+C1+C2
+    right = right.to_qcode()
+    print(right)
+
+    Er = right.get_encoder()
+    #code = qcode.QCode.from_encoder(Er, k=3)
+
+
+    Er = SymplecticSpace(cube.m * n).get_identity() << Er
+    print(Er.shape)
+    #print(Er)
+
+    if 0:
+        code = qcode.QCode.from_encoder(Er, k=3)
+        print(code)
+        code = code.to_css()
+        code.bz_distance()
+        print(code)
+
+    #return
+
+    E = cube.get_encoder()
+
+    El = reduce(lshift, [E]*n)
+    print(El.shape)
+
+    idxs = []
+    for i in range(n):
+      for j in range(cube.m):
+        idxs.append(cube.n*i + j)
+
+    N = cube.m*n
+    for i in range(cube.k):
+      for j in range(n):
+        idxs.append(cube.n*j + cube.m + i)
+
+    #print(idxs)
+
+    assert len(set(idxs)) == len(idxs)
+    assert set(idxs) == set(range(len(idxs)))
+
+    assert len(idxs)*2 == len(El)
+    assert len(idxs) == n * cube.n
+    P = SymplecticSpace(n*cube.n).get_perm(idxs).t
+    E = El * P * Er
+    code = qcode.QCode.from_encoder(E, k=3)
+    d = code.distance("z3")
+    print(code, d)
+    
+    #print(code.longstr())
+
+    #return
+
+    code = code.to_css()
+    code.bz_distance()
+    print(code)
+
+    from qumba.gcolor import dump_transverse
+    dump_transverse(code.Hx, code.Lx)
+
+    
 
 
 if __name__ == "__main__":
