bd6a146e36661bcc5e23cd3f843f0fc286d50034
Simon Burton <simon@arrowtheory.com>
2026-06-01 11:06:34 +0100

    commit edits

---
+++ qumba/phase_poly.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+
+"""
+
+Analyse diagonal gates using phase polynomials
+
+"""
+
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+from math import gcd
+from functools import reduce, cache
+from operator import matmul, add, mul
+from random import random, randint, choice, shuffle
+
+import z3
+
+import numpy
+
+from sage import all_cmdline as sage
+
+from qumba.argv import argv
+from qumba.qcode import strop, QCode
+from qumba.csscode import CSSCode
+from qumba import construct 
+from qumba.matrix import Matrix
+
+#from qumba.matrix_sage import Matrix
+#from qumba.clifford import Clifford, w8, w4, r2, ir2, half
+#from qumba import clifford 
+#from qumba.action import mulclose
+#from qumba.lin import shortstr
+#from qumba.distill import PauliDistill
+
+
+class Op:
+    def __init__(self, items={}, level=1):
+        items = dict(items)
+        assert 0<=level
+        self.items = items
+        self.level = level
+
+    def __str__(self):
+        return "Op(%s, %s)"%(self.items, self.level)
+
+    def __eq__(self, other):
+        assert self.level == other.level
+        return self.items == other.items
+
+    def __mul__(self, other):
+        assert self.level == other.level
+        level = self.level
+        N = 2**level
+        items = dict(self.items)
+        for idxs,v in other.items.items():
+            if idxs in items:
+                v = (items[idxs] + v) % N
+                if v == 0:
+                    del items[idxs]
+                else:
+                    items[idxs] = v
+            else:
+                items[idxs] = v
+        return Op(items, level)
+
+    def __pow__(self, n):
+        op = Op({}, self.level)
+        for i in range(n):
+            op = op*self
+        return op
+
+
+class Diag:
+    def __init__(self, n, level=1):
+        self.n = n
+        self.level = level
+
+    def get_identity(self):
+        return Op({}, self.level)
+
+    def op(self, i, colevel=1):
+        level = self.level
+        assert level >= colevel
+        N = 2**(level-colevel)
+        items = {(i,):N}
+        return Op(items, level)
+
+    def cop(self, i, j, colevel=1):
+        assert i!=j
+        level = self.level
+        assert level >= colevel
+        if j<i:
+            i, j = j, i
+        N = 2**(level-colevel)
+        items = {(i,j):N}
+        return Op(items, level)
+
+    def ccop(self, i, j, k, colevel=1):
+        level = self.level
+        assert level >= colevel
+        idxs = [i, j, k]
+        idxs.sort()
+        i, j, k = idxs
+        assert i!=j and j!=k
+        N = 2**(level-colevel)
+        items = {(i,j,k):N}
+        return Op(items, level)
+
+    def Z(self, i):
+        return self.op(i, 1)
+
+    def CZ(self, i, j):
+        return self.cop(i, j, 1)
+
+    def CCZ(self, i, j, k):
+        return self.ccop(i, j, k, 1)
+
+    def S(self, i):
+        return self.op(i, 2)
+
+    def CS(self, i, j):
+        return self.cop(i, j, 2)
+
+    def CCS(self, i, j, k):
+        return self.ccop(i, j, k, 2)
+
+    def T(self, i):
+        return self.op(i, 3)
+
+    def CT(self, i, j):
+        return self.cop(i, j, 3)
+
+    def CCT(self, i, j, k):
+        return self.ccop(i, j, k, 3)
+
+    def R(self, i):
+        return self.op(i, 4)
+
+    # etc...
+
+
+def test():
+
+    #code = construct.get_832()
+    code = construct.get_713()
+
+    n = code.n
+
+    space = Diag(n, 2)
+    Z = space.Z
+    S = space.S
+    CS = space.CS
+    CZ = space.CZ
+    I = space.get_identity()
+    Z2 = Z(2)
+    Z3 = Z(3)
+
+    assert Z2*Z2 == I
+    assert Z2*Z3 != I
+    assert Z2*Z3 == Z3*Z2
+
+    assert CZ(2,0) == CZ(0,2)
+    s = str(CZ(2,0) * Z(3) * S(2))
+
+    g = S(2)
+    assert g**0 == I
+    assert g != I
+    assert g*g != I
+    assert g*g*g == g**3
+    assert g**3 != I
+    assert g**4 == I
+
+
+    print(code)
+    print(code.longstr())
+
+    css = code.to_css()
+    Hx = Matrix(css.Hx)
+
+    for v in Hx.span():
+        print(v)
+
+
+
+
+if __name__ == "__main__":
+
+    from random import seed
+    from time import time
+    start_time = time()
+
+    profile = argv.profile
+    name = argv.next() or "test"
+    fn = eval(name)
+
+    _seed = argv.get("seed")
+    if _seed is not None:
+        print("seed(%s)"%(_seed))
+        seed(_seed)
+
+    if profile:
+        from pyinstrument import Profiler
+        with Profiler(interval=0.01) as profiler:
+            fn()
+        profiler.print()
+
+    else:
+        fn()
+
+
+    t = time() - start_time
+    print("\nOK! finished in %.3f seconds\n"%t)
+
+
