b91bd69d081106d8c0bae50f9b0539903425fca2
Simon Burton <simon@arrowtheory.com>
2026-08-29 11:35:29 +0100

    commit edits

--- qumba/hecke.py
+++ qumba/hecke.py
@@ -19,9 +19,9 @@ from qumba.csscode import CSSCode
 def span_ops(ops):
     N = len(ops)
     m, n = ops[0].shape
-    if N >= 10:
+    if N > 12:
+        print("(span_ops: N=%d is too big?)" % N, end=" ", flush=True)
         return
-    assert N < 10, N
     for bits in numpy.ndindex((2,)*N):
         if sum(bits) == 0:
             continue
@@ -36,21 +36,28 @@ def span_ops(ops):
 found = set()
 
 
-def get_equi(G):
+def get_selfdual(G, desc=None):
 
-    print(G, G.structure_description())
+    s = G.structure_description()
+    if desc and s != desc:
+        return
+
+    print("\t", G, G.structure_description(), end=", ")
 
     #print(G.perms)
     N = G.rank
     n = gap.Order(G, get=True)
     assert len(G) == n
-    #_Hs = gap.AllSubgroups(G)
-    _Hs = gap.ConjugacyClassesSubgroups(G)
+    if argv.all_subgroups:
+        _Hs = gap.AllSubgroups(G)
+    else:
+        _Hs = gap.ConjugacyClassesSubgroups(G)
     #print("\t", gap.Length(Hs, get=True))
     Hs = []
     for i in range(len(_Hs)):
         item = _Hs[i]
-        item = gap.Representative(item)
+        if not argv.all_subgroups:
+            item = gap.Representative(item)
         assert gap.IsPermGroup(item, get=True)
         H = gap.to_group(item, N=N)
         #print("\t", H)
@@ -69,13 +76,12 @@ def get_equi(G):
         #print(ops[0].shape, end=' ')
         ops = [Matrix(op) for op in ops]
 
-        #m, n = ops[0].shape
-        #if m!=21 and n!=21:
-        #    continue
-
-        #for H0 in ops:
         for H0 in span_ops(ops):
           for H in [H0, H0.t]:
+            _, n = H.shape
+            #if n <= 16: # this code is too small ...
+            #    continue
+
             HHt = H*H.t # overlap of the rows
             if HHt.sum() != 0:
                 continue
@@ -87,33 +93,109 @@ def get_equi(G):
                 continue
             #print(HHt.shape)
             #print(H.shortstr(), H.shape)
-            css = CSSCode(Hx=H, Hz=H)
-            if css.k:
+            css = CSSCode(Hx=H, Hz=H, check=False)
+            if css.n-css.k < 100:
                 css.bz_distance()
-            if css.d <= 2:
+            if css.d and css.d <= 2:
                 continue
-            #if css.n > 20 and css.d <= 4:
-            #    continue
 
-            #print(css, H.shape)
-            s = str(css)
-            if css.d > 6:
-                s += "FOUND!"
+            if (css.n, css.k, css.d) == (24, 14, 4):
+                print(H, css)
 
-            if s not in found:
-                found.add(s)
-                print(s)
+            if len(H) > 20:
+                key = (css.n, css.k, css.d)
+                wenum = None
 
-#        smap = SMap()
-#        col = 0
-#        for op in ops:
-#            smap[0, col] = op.shortstr()
-#            col += op.shape[1] + 1
-#        #smap[op.shape[0], 0] = "-"*col
-#        print(smap)
-#        print()
-#
-#      print()
+            else:
+                wenum = H.get_wenum()
+                key = (css.d, wenum)
+
+            if key not in found:
+                found.add(key)
+                weight = 0
+                if wenum is not None:
+                  for idx,wt in enumerate(wenum):
+                    if idx and wt:
+                        weight = idx
+                        break
+                print(css, weight)
+
+
+def get_css(G, desc=None):
+
+    s = G.structure_description()
+    if desc and s != desc:
+        return
+
+    print("\t", G, G.structure_description(), end=", ")
+
+    #print(G.perms)
+    N = G.rank
+    n = gap.Order(G, get=True)
+    assert len(G) == n
+    _Hs = gap.ConjugacyClassesSubgroups(G)
+    #print("\t", gap.Length(Hs, get=True))
+    Hs = []
+    for i in range(len(_Hs)):
+        item = _Hs[i]
+        item = gap.Representative(item)
+        assert gap.IsPermGroup(item, get=True)
+        H = gap.to_group(item, N=N)
+        #print("\t", H)
+        assert G.is_subgroup(H)
+        if len(G) > len(H) > 1: # right ?!?
+            Hs.append(H)
+
+    del N
+    print("subgroups:", len(Hs), end=', ', flush=True)
+
+    print("build action... ", end='', flush=True)
+    Xs = [G.left_action(H) for H in Hs]
+
+    N = len(Xs)
+    pairs = {}
+    for i in range(N):
+      for j in range(N):
+        ops = list(get_operators(Xs[i].gens, Xs[j].gens))
+        ops = [Matrix(op) for op in ops]
+        ops = [H.linear_independent() for H in span_ops(ops)]
+        pairs[i,j] = ops
+
+    print("done")
+
+    for i in range(N):
+     for jx in range(N):
+       opxs = pairs[jx, i]
+       for jz in range(N):
+        opzs = pairs[jz, i]
+
+        for Hx in (opxs):
+         for Hz in (opzs):
+
+            HHt = Hx*Hz.t
+            if HHt.sum() != 0:
+                continue
+
+            #Hx = Hx.linear_independent()
+            #Hz = Hz.linear_independent()
+            mx, n = Hx.shape
+            mz, n = Hz.shape
+            assert mx+mz <= n
+            if mx+mz == n:
+                continue
+            css = CSSCode(Hx=Hx, Hz=Hz, check=False, build=False)
+            if css.n-css.k < 100:
+                css.bz_distance()
+            if css.d and css.d <= 2:
+                continue
+
+            wx, wz = (Hx.get_wenum(), Hz.get_wenum())
+            if (wx,wz) in found or (wz,wx) in found:
+                continue
+
+            found.add((wx,wz))
+            print(css)
+
 
 
 
@@ -121,36 +203,98 @@ def main():
 
     gap = Gap()
 
-    if argv.GL:
+    func = get_selfdual
+    if argv.get_css:
+        func = get_css
+
+    print(func)
+
+    if argv.PSL:
         m = argv.get("m", 3)
         n = argv.get("n", 2)
         N = argv.get("N", None)
         _G = gap.PSL(m, n)
         G = gap.to_group(_G, smaller=False, N=N)
-        get_equi(G)
+        func(G)
         return
 
     if argv.Alternating:
         n = argv.get("n", 5)
         _G = gap.AlternatingGroup(n)
         G = gap.to_group(_G, smaller=False)
-        get_equi(G)
+        func(G)
+        return
+
+    if argv.Symmetric:
+        n = argv.get("n", 5)
+        _G = gap.SymmetricGroup(n)
+        G = gap.to_group(_G, smaller=False)
+        func(G)
+        return
+
+    if argv.CoxeterBC:
+        n = argv.get("n", 3)
+        G = Group.coxeter_bc(n)
+        func(G)
+        return
+
+    if argv.CoxeterD:
+        n = argv.get("n", 4)
+        G = Group.coxeter_d(n)
+        func(G)
         return
 
     if argv.Mathieu:
         n = argv.get("n", 11)
         _G = gap.MathieuGroup(n)
         G = gap.to_group(_G, smaller=False)
-        get_equi(G)
+        func(G)
         return
 
-    n0 = argv.get("n0", 33)
-    n1 = argv.get("n1", 60)
+    desc = argv.desc
+    n = argv.get("n", 27)
+    n0 = argv.get("n0", n)
+    n1 = argv.get("n1", n0+1)
     for _G in allgroups(n0, n1):
         G = gap.to_group(_G, smaller=False)
-        get_equi(G)
+        func(G, desc)
 
 
+
+def test_24():
+    H = Matrix.parse("""
+    11..1..11.1.11..1.1.1..1
+    ..11.11.1.1...111.1..11.
+    ..111..1.1.1..11.1.11..1
+    1..11.1.11...11.11..1.1.
+    1.1.11..1..11.1..11...11 
+    """)
+
+    H = Matrix.parse("""
+    ..11......111.1.1.1.
+    .11.....1..11..1..11
+    1..111...1.1....1.1.
+    11..1.1.1.1..11.....
+    ......11..11.11..11.
+    ....11111..11.1.1..1
+    """)
+    
+
+    w = H.get_wenum()
+    print(w)
+
+    N, perms = H.get_autos()
+    perms = [Perm(idxs) for idxs in perms]
+    G = Group.generate(perms)
+    print(len(G))
+    print(G.structure_description())
+
+    from bruhat.gap import Gap
+    gap = Gap()
+
+    M = gap.MathieuGroup(24)
+    _G = gap.define(G)
+    print(gap.IsomorphicSubgroups(M, _G, get=True)) # yes
 
 
 
